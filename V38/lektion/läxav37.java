70a3669802e866c7fb0bfb865672a225657f31d1
emilgus02 <emil.gustafsson@elev.ga.ntig.se>
2019-09-23 07:32:19 +0200

    Create läxav37.java

---
+++ V38/lektion/läxav37.java
@@ -0,0 +1,64 @@
+package lektion;
+
+import java.util.Scanner;
+
+public class läxav37 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		int tal1 = 0;
+		int tal2 = 0;
+		int tal3 = 0;
+		int tal4 = 0;
+		int tal5 = 0;
+		double dela = 5;
+
+		System.out.println("Skriv 5 heltal:");
+		tal1 = input.nextInt();
+		tal2 = input.nextInt();
+		tal3 = input.nextInt();
+		tal4 = input.nextInt();
+		tal5 = input.nextInt();
+		double medel = (tal1 + tal2 + tal3 + tal4 + tal5) / dela;// räknar ut medelvärdet
+
+		if (tal1 >= tal2 && tal1 >= tal3 && tal1 >= tal4 && tal1 >= tal5) {
+			
+			System.out.println("Störst:" + "\t" + tal1);
+		}
+		else if (tal2 >= tal1 && tal2 >= tal3 && tal2 >= tal4 && tal2 >= tal5) {
+			System.out.println("Störst:" + "\t" + tal2);
+		}
+		
+		else if (tal3 >= tal1 && tal3 >= tal2 && tal3 >= tal4 && tal3 >= tal5) {
+			System.out.println("Störst:" + "\t" + tal3);			
+		}
+		else if (tal4 >= tal1 && tal4 >= tal3 && tal4 >= tal3 && tal4 >= tal5) {
+			System.out.println("Störst:" + "\t" + tal4);
+		}
+		else if (tal5 >= tal1 && tal5 >= tal3 && tal5 >= tal3 && tal5 >= tal4) {
+			System.out.println("Störst:" + "\t" + tal5);
+		};
+		
+		
+		if  (tal1 <= tal2 && tal1 <= tal3 && tal1 <= tal4 && tal1 <= tal5) {
+			System.out.println("Minst:" + "\t" + tal1);
+		}
+		else if (tal2 <= tal1 && tal2 <= tal3 && tal2 <= tal4 && tal2 <= tal5) {
+			System.out.println("Minst:" + "\t" + tal2);
+		}
+		else if (tal3 <= tal1 && tal3 <= tal2 && tal3 <= tal4 && tal3 <= tal5) {
+			System.out.println("Minst:" + "\t" + tal3);
+			}
+		else if (tal4 <= tal1 && tal4 <= tal3 && tal4 <= tal2 && tal4 <= tal5) {
+			System.out.println("Minst:" + "\t" + tal4);
+			}
+		else if (tal5 <= tal1 && tal5 <= tal3 && tal5 <= tal4 && tal5 <= tal2) {
+			System.out.println("Minst:" + "\t" + tal5);
+			}
+		System.out.println("medelvärde :" + medel);
+
+	}
+
+}
