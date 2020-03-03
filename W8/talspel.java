42ed3c8b7f22a4417aacc38a6831afcd3eab1090
emilgus02 <emil.gustafsson@elev.ga.ntig.se>
2020-03-03 11:59:28 +0100

    läxa

--- W8/talspel.java
+++ W8/talspel.java
@@ -12,76 +12,117 @@ public class talspel {
 		boolean igen = true;
 		String choice;
 
-		while (igen == true) {
+		do {
 			System.out.println("Välkommen till Guess The Number");
 			System.out.println("Här kan du välja svighetsgrad på spelet");
 			System.out.println("(1) Lätt: 15 Gissningar");
 			System.out.println("(2) Medel: 10 Gissningar");
 			System.out.println("(3) Svår: 5 Gissningar");
-			System.out.println("(4) Master: 1 Gissning");
+			System.out.println("(4) Eget: Välj egna intervall");
 			System.out.println("(5) Avsluta");
 			System.out.println();
 
 			choice = input.nextLine();
 
-			if (choice.equals("1")) { // Kollar om man har skrivit 1
-				int antal = 15; // väljer hur många antal gissningar man har
-				guess(igen, antal); // Kallar metoden guess
-			} else if (choice.equals("2")) {
-				int antal = 10;
-				guess(igen, antal);
-			} else if (choice.equals("3")) {
-				int antal = 5;
-				guess(igen, antal);
-			} else if (choice.equals("4")) {
-				master(igen);
-			} else if (choice.equals("5")) {
-				System.exit(0); // Stänger av programmet
-			} else { // Om man inte skrivit ett tal mellan 1-5
-				System.out.println("Snälla skriv ett tal mellan 1 och 4 tack");
-			}
-		}
+			difficulty(choice, igen);
+		} while (igen == true);
 	}
 
 	/**
-	 * Här gör man också en gissning bara att det är en annan svårhetes grad så man
-	 * får bara en chans på sig
+	 * Väljer svårighetsgrad
 	 * 
+	 * @param choice
 	 * @param igen
+	 */
+	private static void difficulty(String choice, boolean igen) {
+		Scanner input = new Scanner(System.in);
+		if (choice.equals("1")) { // Kollar om man har skrivit 1
+			int antal = 15;// väljer hur många antal gissningar man har
+			int max = 100;
+			guess(igen, antal, max); // Kallar metoden guess
+		} else if (choice.equals("2")) {
+			int antal = 10;
+			int max = 100;
+			guess(igen, antal, max);
+		} else if (choice.equals("3")) {
+			int antal = 5;
+			int max = 100;
+			guess(igen, antal, max);
+		} else if (choice.equals("4")) {
+			int max = 0;
+			int antal = 0;
+			String max1;
+			boolean diff0 = false;
+			boolean diff = false;
+			boolean diff2 = false;
+			boolean diff3 = false;
+			do { // loopar om man skriver in 0
+				diff0 = false;
+				System.out.println("Hur många gissningar vill du ha? (Skriv gärna ett tal mellan 1 och 999999999)");
+				do { // om man skriver en bokstav
+					diff = false;
+					max1 = input.nextLine();
+					if (nummer2(max1) == true) { // Kollar om det är ett heltal
+						antal = Integer.parseInt(max1); // Gör om talet till en Int
+
+					} else {
+						System.out.println("Skriv gärna ett heltal mellan 1 och 999999999");
+						diff = true;
+					}
+				} while (diff == true);
+				System.out.println(
+						"Vad ska största talet i intervallet vara? (Skriv gärna ett tal mellan 1 och 999999999)");
+				do { // Om man skriver en bokstav
+					diff2 = false;
+					max1 = input.nextLine();
+					if (nummer2(max1) == true) {
+						max = Integer.parseInt(max1); // Gör om max1 till int max
+					} else {
+						System.out.println("Skriv gärna ett heltal mellan 1 och 999999999");
+						diff2 = true;
+					}
+				} while (diff2 == true);
+				if (max > 0 && antal > 0) { // Kollar så talet inte är under 1
+					System.out.println();
+					guess(igen, antal, max);
+				} else if (max == 0) {
+					System.out.println("Du kan inte ha 0 som max i intervallet");
+					diff0 = true;
+				} else {
+					System.out.println("Du kan inte ha 0 gissningar");
+					diff0 = true;
+				}
+			} while (diff0 == true);
+		} else if (choice.equals("5")) {
+			System.exit(0); // Stänger av programmet
+		} else { // Om man inte skrivit ett tal mellan 1-5
+			System.out.println("Snälla skriv ett tal mellan 1 och 4 tack");
+		}
+
+	}
+
+	/**
+	 * Kollar om talet är ett heltal om det är under 999999999
+	 * 
+	 * @param max1
 	 * @return
 	 */
-	private static boolean master(boolean igen) {
-		System.out.println("Du valde Master");
-		System.out.println("Gissa nu ett tal mellan 1-100");
-		Scanner input = new Scanner(System.in);
-		int talet = 0;
-		boolean car = true;
-		int slump = (int) (Math.random() * (100) + 1);
-		do {
-			String spel = input.nextLine();
-			if (nummer(spel) == true) {
-				talet = Integer.parseInt(spel);
-				if (talet > 0 && talet < 101) {
-					if (talet == slump) { // Kollar om man har rätt
-						System.out.println("Du vann GRATTIS!!! du är nu är Master ");
-						System.out.println("Vill du spela igen? Skriv 1 om du vill spela igen 0 om du inte");
-						car = false;
-					} else {
-						System.out.println("Du har inga gissningar kvar, talet var " + slump);
-						System.out.println("Vill du spela igen? Skriv 1 om du vill spela igen 0 om du inte");
-						car = false;
-					}
-				} else {
-					System.out.println("Välj gärna ett heltal mellan 1-100");
-				}
-			} else {
-				System.out.println("Välj gärna ett heltal mellan 1-100");
+	private static boolean nummer2(String max1) {
+		int length = max1.length(); // Skapar en inte som är längden på Stringen spel som sedan används i for loopen
+		if (length > 9) { // Kollar så talet är inte större än 999 999 999. Används mer för att det inte
+							// ska bli
+							// error när man går över intens storlek
+			return false;
+		}
+		if (max1.isEmpty()) { // Kollar om man ar skrivit något i max1
+			return false;
+		}
+		for (int i = 0; i < length; i++) {
+			if (Character.isDigit(max1.charAt(i)) == false) { // isDigit kollar om det bara i siffror i spel
+				return false;
 			}
-		} while (car == true);
-
-		igen1(igen); // Man hamnar i metoden igen1 för att kolla om man vill köra igen
-
-		return igen; // Returnar igen om man vill köra igen
+		}
+		return true;
 	}
 
 	/**
@@ -92,23 +133,23 @@ public class talspel {
 	 * @param antal
 	 * @return
 	 */
-	private static boolean guess(boolean igen, int antal) {
+	private static void guess(boolean igen, int antal, int max) {
 		System.out.println("Du har nu valt en svårighetsgrad");
-		System.out.println("Gissa nu ett tal mellan 1-100");
+		System.out.println("Gissa nu ett tal mellan " + 1 + "-" + max);
 		Scanner input = new Scanner(System.in);
 		int talet = 0;
-		int slump = (int) (Math.random() * (100) + 1); // Det här slumpar talet man ska gissa
-
+		int slump = (int) (Math.random() * (max) + 1); // Det här slumpar talet man ska gissa
 		for (int i = 0; i <= antal; i++) { // for loopen är för att kolla hur många chanser man har på sig
 			int k = antal - i; // är för att visa hur många chanser man har kvar
 			if (i == antal) { // Här hamnar man om man inte har några gissningar kvar
 				System.out.println("Du har inga gissningar kvar, talet var " + slump);
-				System.out.println("Vill du spela igen? Skriv 1 om du vill spela igen 0 om du inte");
 				igen1(igen); // man hamnar i metoden igen1 för att kolla om man vill köra igen
 			} else {
 				System.out.println("Du har " + k + " gissningar kvar");
+				System.out.println();
 				String spel = input.nextLine(); // Här gör man gissningen om man vilket tal man tror att det är
-				if (nummer(spel) == true) { // Man hamnar i metoden nummer för att kolla att det är endast ett heltal i
+				if (nummer(spel) == true) { // Man hamnar i metoden nummer för att kolla att det är endast ett
+											// heltal i
 											// och inte någon bokstav
 					talet = Integer.parseInt(spel); // Detta gör om Stringen till en int
 
@@ -116,11 +157,10 @@ public class talspel {
 											// vunnit
 						int L = k - 1; // För att visa hur många försök man hade kvar när man klara det
 						System.out.println("Du vann GRATTIS!!! du hade " + L + " försök kvar");
-						System.out.println("Vill du spela igen? Skriv 1 om du vill spela igen 0 om du inte");
 						igen1(igen);
 						i = antal; // För att stoppa loopen
 					} else { // Om man inte gissar rätt
-						if (talet > 0 && talet < 101) {// Om talet är inte mellan 1-100 så körs inte denna loop
+						if (talet > 0 && talet < (max + 1)) {// Om talet är inte mellan 1-max så körs inte denna loop
 							if (slump > talet) { // Om talet är större än rätt svar
 								System.out.println("Talet är större än ditt tal");
 							} else if (slump < talet) { // Om talet är mindre är rätt tal
@@ -129,17 +169,16 @@ public class talspel {
 						} else {
 							i--; // För att man ska få en till chans om man har gissat ett tal som inte är mellan
 									// 1-100
-							System.out.println("Välj ett tal mellan 1-100");
+							System.out.println("Välj ett tal mellan " + 1 + "-" + max);
 						}
 					}
 				} else {
-					System.out.println("Välj gärna ett heltal mellan 1-100");
+					System.out.println("Välj ett tal mellan " + 1 + "-" + max);
 					i--; // För att man ska få en till chans om man har gissat med en bokstav
 				}
 			}
 
 		}
-		return igen; // Returnar igen om man vill köra igen
 	}
 
 	/**
@@ -150,6 +189,10 @@ public class talspel {
 	 */
 	private static boolean nummer(String spel) {
 		int length = spel.length(); // Skapar en inte som är längden på Stringen spel som sedan används i for loopen
+		if (length > 3) { // Kollar så talet är inte större än 100. Används mer för att det inte ska bli
+							// error när man går ver intens storlek
+			return false;
+		}
 		if (spel.isEmpty()) { // Kollar om man ar skrivit något i spel
 			return false;
 		}
@@ -167,13 +210,13 @@ public class talspel {
 	 * @param igen
 	 * @return
 	 */
-	private static boolean igen1(boolean igen) {
+	private static void igen1(boolean igen) {
 		Scanner input = new Scanner(System.in);
 		boolean boll = true;
+		System.out.println("Vill du spela igen? Skriv 1 om du vill spela igen 0 om du inte");
 		do {
 			String again = input.nextLine();
 			if (again.equals("1")) { // Kollar om man har skrivit 1
-				igen = true; // säger att man vill köra igen
 				boll = false; // stänger av den här loopen
 			} else if (again.equals("0")) { // Kollar om man har skrivit 2
 				System.exit(0); // Stänger av programmet
@@ -181,7 +224,6 @@ public class talspel {
 				System.out.println("Snälla välj 1 eller 0");
 			}
 		} while (boll == true);
-		return igen; // Returnar igen för att man för att veta om man vill köra igen eller inte
 	}
 
 }
--- W8/testaaaa.java
+++ W8/testaaaa.java
@@ -5,14 +5,37 @@ import com.sun.xml.internal.ws.util.StringUtils;
 public class testaaaa {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		String ord = input.next();
-		if (StringUtils.capitalize(ord) == null) {
-			System.out.println("rätt");
-		}
-		else {
+
+		String max = box();
+		
+		if(random(max) == true) {
+			System.out.println(max);
+		} else {
 			System.out.println("fel");
 		}
 	}
 
+	private static boolean random(String max) {
+		int length = max.length(); // Skapar en inte som är längden på Stringen spel som sedan används i for loopen
+		if (length > 10) { // Kollar så talet är inte större än 100. Används mer för att det inte ska bli
+							// error när man går ver intens storlek
+			return false;
+		} 
+		if (max.isEmpty()) { // Kollar om man ar skrivit något i spel
+			return false;
+		}
+		for (int i = 0; i < length; i++) {
+			if (Character.isDigit(max.charAt(i)) == false) { // isDigit kollar om det bara i siffror i spel
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static String box() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("skriv nptt");
+		String diff = input.nextLine();
+		return diff;
+	}
 }
